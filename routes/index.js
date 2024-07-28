7d2f1685384c586e8579d5182604efd3ec286d5d
unknown <travihay@gmail.com>
2024-07-28 18:05:07 -0500

    changed paths

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const router = express.Router(); // Creating a new router instance using Express
 
 // Route to serve the notes.html page
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../notes.html')); // Sending notes.html file as response
+  res.sendFile(path.join(__dirname, '/..public/notes.html')); // Sending notes.html file as response
 });
 
 // Route to serve the index.html page for all other routes
 router.get('/html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html')); // Sending index.html file as response
+  res.sendFile(path.join(__dirname, '../public/index.html')); // Sending index.html file as response
 });
 
 module.exports = router; // Exporting the router instance for use in other files
